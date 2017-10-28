c95b8c7eae092513988d07aaecd4abfd72e9f02a
bestsuperweb <bestsuperweb@gmail.com>
2017-10-28 16:22:09 -0400

    modify index.php

--- index.php
+++ index.php
@@ -1,31 +1,97 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    
-    <title>Lenox | Home</title>
-    <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
-    
-    <!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+  <title>Lenox | Home</title>
+  <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
 
-    <!-- Custom CSS -->
-    <link rel="stylesheet" type="text/css" href="assets/css/animate.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/hover-min.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/index.css" >
-    
-  </head>
-  <body>
-    
-    
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="assets/js/wow.min.js"></script>
-    <!-- Custom JS -->
-	<script src="assets/js/js.js"></script>
-  </body>
+  <!-- Custom CSS -->
+  <link rel="stylesheet" type="text/css" href="assets/css/animate.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/hover-min.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/index.css" >
+
+</head>
+<body>
+  <nav class="navbar" w3-include-html="shared/header.html"></nav>
+
+  <section id="home-banner">
+    <video src="assets/video/lenoxapts.mp4" autoplay="true" loop="true">      
+    </video>
+    <div class="home-tagline text-center">
+      <div class="tagline-item">
+        <h1 class="wow zoomIn">
+          “Harlem’s Best Address”        
+        </h1>
+        <p class="wow fadeInUp">The New York Times</p>
+      </div>
+      <div class="tagline-item">
+      <h1 class="wow bounceInDown">
+          Harlem’s Finest No Fee Rental Apartments      
+        </h1>
+      </div>
+      <div class="tagline-item">
+        <h1 class="wow zoomIn">
+          More Than Just Living  
+        </h1>
+      </div>
+    </div>
+  </section>
+
+  <section id="home-items">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-6 col-lg-3">
+          <img src="assets/img/home/first_img.jpg" class="img-responsive">
+          <div class="item">
+            <h1>Living</h1>
+            <hr class="line-under">
+            <p>Discover The Residences</p>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-3">
+          <img src="assets/img/home/second_img.jpg" class="img-responsive">
+          <div class="item">
+            <h1>Luxuries</h1>
+            <hr class="line-under">
+            <p>View Features</p>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-3">
+          <img src="assets/img/home/third_img.jpg" class="img-responsive">
+          <div class="item">
+            <h1>Attractions</h1>
+            <hr class="line-under">
+            <p>Explore Neighborhood</p>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-3">
+          <img src="assets/img/home/fourth_img.jpg" class="img-responsive">
+          <div class="item">
+            <h1>Vacancies</h1>
+            <hr class="line-under">
+            <p>Check Availability</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <footer w3-include-html="shared/footer.html"></footer>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="assets/js/wow.min.js"></script>
+  <script src="https://www.w3schools.com/lib/w3.js"></script>
+  
+  <!-- Custom JS -->
+  <script src="assets/js/js.js"></script>
+</body>
 </html>
